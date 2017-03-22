81e6503a76550b3a239802e6aac2bea7c2525e66
shashkambh <shashankk@utexas.edu>
2017-03-21 23:45:17 -0500

    edited rss managing

--- scripts/rss.js
+++ scripts/rss.js
@@ -3,13 +3,16 @@ var request = require("request");
 var CronJob = require('cron').CronJob;
 var dbinterface = require('./database.js');
 
+/* Sends an HTTP request to an RSS feed specified by the parameter.
+ * Then sends the updated feed data to the database.
+ */
 function updateFeed(site){
     var req = request(site);
     var fp = new FeedParser();
 
     req.on("response", function(res){
         var stream = this;
-        if(res.statusCode != 200){
+        if(res.statusCode !== 200){
             this.emit('error', new Error('failed'));
         } else {
             stream.pipe(fp);
@@ -23,15 +26,26 @@ function updateFeed(site){
         var items = [];
         
         while (item = stream.read()){
-            items.unshift({title: item.title, link: item.link});
+            items.push({title: item.title, link: item.link});
         }
+4
 
-        dbinterface.addFeed(items);
+        dbinterface.updateArticleList(site, items);
     });
 }
 
-function updateAllFeeds(){
-    updateFeed('http://xkcd.com/rss.xml');
+/* Function passed into getFeedLinks, called with list of feed names
+ * Calls updateFeed on every feed in the database
+ */
+function updateFeeds(feeds, err){
+	if(err) return;
+	
+    feeds.forEach(function(element){
+        updateFeed(element);
+    });
 }
 
-new CronJob('0 0 * * * *', updateAllFeeds, null, true, 'America/Chicago');
+/* Updates all feeds every hour */
+new CronJob('0 0 * * * *', function(){
+	dbinterface.getFeedLinks(updateAllFeeds);
+}, null, true, 'America/Chicago');
